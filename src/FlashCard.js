309cc178ac5f48f8bfbee59d2f3a85b4f3c9363f
catalina <cdianacatalina@gmail.com>
2021-12-14 23:40:20 +0100

    organized code

--- src/FlashCard.js
+++ src/FlashCard.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import "./Card.css";
-import { Grid, CardActionArea, Card, Typography } from "@mui/material";
+import { CardActionArea, Card, Typography } from "@mui/material";
 
-const FlashCard = ({ card }) => {
-  const [cardSide, setCardSide] = useState("front");
+const FlashCard = ({ currentCard, show }) => {
+  console.log("render flashcard");
+  const [cardSide, setCardSide] = useState(show);
+  console.log(cardSide);
 
   /**
    * Toogle the card
@@ -12,12 +14,15 @@ const FlashCard = ({ card }) => {
     cardSide === "front" ? setCardSide("back") : setCardSide("front");
   }
   return (
-    <CardActionArea onClick={toogleCard} sx={{width:"fit-content", height: "fit-content", margin:'auto'}}>
-      <Card sx={{ maxWidth: "300px", padding: "20px" }}>
+    <CardActionArea
+      onClick={toogleCard}
+      sx={{ width: "fit-content", height: "fit-content", margin: "20px auto" }}
+    >
+      <Card sx={{ maxWidth: "300px", maxHeight: "200px", padding: "20px" }}>
         {cardSide === "front" ? (
-          <Typography variant="body1">{card.deu}</Typography>
+          <Typography variant="body1">{currentCard.deu}</Typography>
         ) : (
-          <Typography variant="body1">{card.eng}</Typography>
+          <Typography variant="body1">{currentCard.eng}</Typography>
         )}
       </Card>
     </CardActionArea>
--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,11 @@
-import "./Home.css";
 import React, { useState, useEffect } from "react";
 import FlashCard from "./FlashCard";
-import Button from "./Button";
+import "./Home.css";
 // cloud Firestore
 import { app } from "./configFirebase";
 import { getFirestore } from "firebase/firestore";
 import { collection, onSnapshot } from "firebase/firestore";
-import { addToDB, getCards, addToDoc, deletesDocu, queryTrial } from "./Utility";
+import { Button, CircularProgress } from "@mui/material";
 
 const db = getFirestore();
 
@@ -16,54 +15,53 @@ function Home() {
   const [loading, setLoading] = useState(true);
   const [currentCard, setCurrentCard] = useState(undefined);
 
-  //------- to use, when data needs to be added to the app ----
-  // useEffect(() => {
-  //   addToDB();
-  // }, []);
-  //------- end to use, when data needs to be added to the app ----
-
-  //------- to use, when data needs to be read it just at mounting the app ----
-  // useEffect(() => {
-  //   getCards().then((cardsFromDB) => {
-  //     setCards(cardsFromDB);
-  //     setLoading(false);
-  //   });
-  // }, []);
-  //------- end to use, when data needs to be read it just at mounting the app----
-
+  /**
+   * Cards content are retrieved from DB and set on cards state.
+   * Once this function is called (when the page is loaded -- useEffect),
+   * it listens to updates made on the retrieved documents. That means if the content
+   * of a document is changed on the database, it will get that content,
+   * making it available to the user on the website
+   */
   function getCardsLisener() {
+    // cards content is collected in cardsFromDB
     let cardsFromDB = [];
     const verbRef = collection(db, "verbs");
-    const unsub = onSnapshot(verbRef, (querySnapshot) => {
+    // onSnapshot method listens to a document
+    const unsubscribe = onSnapshot(verbRef, (querySnapshot) => {
       querySnapshot.forEach((doc) => {
         cardsFromDB.push(doc.data());
       });
+      console.log("cards: ", cardsFromDB);
       setCards(cardsFromDB);
       setLoading(false);
     });
   }
 
+  // Runs everytime that the webpage gets load.
   useEffect(() => {
     getCardsLisener();
   }, []);
-  // useEffect is running everytime webpage gets load -- on-mount.
 
+  // Runs everytime cards is changed.
+  // When the cards are uploaded, update card runs:
+  // picking a card to diplay
   useEffect(() => {
-    console.log("only when setNewCard right!!!!");
     if (cards.length > 0) {
       updateCard();
     }
   }, [cards]);
 
+  /**
+   * Picks a random card to display
+   */
   function getRandomCard() {
     // return a number between 0 - array length
     const randomId = Math.floor(Math.random() * cards.length);
-    console.log("choosing a card:");
-    console.log(cards[randomId]);
     return cards[randomId];
   }
 
   /**
+   * sets current card to be displayed.
    * updates the card, making sure that the last card
    * is diferent to the new one.
    */
@@ -75,41 +73,71 @@ function Home() {
     setCurrentCard(update);
   }
 
-  function addExtraInfo() {
-    addToDoc();
-  }
-
-  function deleteDoc() {
-    deletesDocu();
-  }
-
-  function searchDocs() {
-    queryTrial();
-  }
-
   if (loading || !currentCard) {
     return (
       <div className="App">
-        <img src="spinner.gif" alt="loading" width="50" />
+        <CircularProgress value={25} sx={{ marginTop: "70px" }} />
       </div>
     );
   }
 
   return (
     <div className="App">
-      <h1>Flash cards</h1>
-      <FlashCard card={currentCard} />
-      <Button click={updateCard}> New Card </Button>
-      <div>
-        <Button click={addExtraInfo}> AddExtraInfo </Button>
-      </div>
-      <div>
-        <Button click={deleteDoc}> Delete doc </Button>
-      </div>
-      <div>
-        <Button click={searchDocs}> Search docs </Button>
-      </div>
+      <h2>Flash cards</h2>
+      <FlashCard currentCard={currentCard} show="front" />
+      <Button
+        variant="contained"
+        onClick={updateCard}
+        sx={{ textTransform: "none" }}
+      >
+        New Card
+      </Button>
     </div>
   );
 }
 export default Home;
+
+// import {
+//   addToDB,
+//   getCards,
+//   addToDoc,
+//   deletesDocu,
+//   queryTrial,
+// } from "./Utility";
+
+//------- to use, when data needs to be added to the app ----
+// useEffect(() => {
+//   addToDB();
+// }, []);
+//------- end to use, when data needs to be added to the app ----
+
+//------- to use, when data needs to be read it just at mounting the app ----
+// useEffect(() => {
+//   getCards().then((cardsFromDB) => {
+//     setCards(cardsFromDB);
+//     setLoading(false);
+//   });
+// }, []);
+//------- end to use, when data needs to be read it just at mounting the app----
+
+// function addExtraInfo() {
+//   addToDoc();
+// }
+
+// function deleteDoc() {
+//   deletesDocu();
+// }
+
+// function searchDocs() {
+//   queryTrial();
+// }
+
+/* <div>
+<Button click={addExtraInfo}> AddExtraInfo </Button>
+</div> 
+<div>
+<Button click={deleteDoc}> Delete doc </Button>
+</div> 
+<div>
+<Button click={searchDocs}> Search docs </Button>
+</div> */
